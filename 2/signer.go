4afa673f5301c787a97edccc23a7acdcd9c01263
исканье <iskanderbikta@yandex.ru>
2025-09-06 00:10:01 +0300

    hash funcs

--- 2/signer.go
+++ 2/signer.go
@@ -1,3 +1,34 @@
 package main
 
-// сюда писать код
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func ExecutePipeline(jobs ...job) {
+
+}
+
+func SingleHash(in, out chan interface{}) {
+	outData := <-out
+	in <- DataSignerCrc32(outData.(string)) + "~" + DataSignerCrc32(DataSignerMd5(outData.(string)))
+}
+
+func MultiHash(in, out chan interface{}) {
+	outData := <-out
+	for i := 0; i <= 5; i++ {
+		in <- DataSignerCrc32(strconv.Itoa(i) + outData.(string))
+	}
+}
+
+func CombineResults(in, out chan interface{}) {
+	var result []string
+
+	for i := range out {
+		result = append(result, i.(string))
+	}
+
+	sort.Strings(result)
+	in <- strings.Join(result, "_")
+}
